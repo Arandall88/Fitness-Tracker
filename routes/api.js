845e1961ce5b165914fb5473784a3b806762b666
Angel Randall <drivenbysuccess21@gmail.com>
2020-04-24 15:31:01 -0500

    updated api.jjs

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
 router.post("/api/workouts", (req, res) => {
-  Workout.create()
+  Workout.create({})
     .then((dbWorkouts) => {
       res.json(dbWorkouts);
     })
