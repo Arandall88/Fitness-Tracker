a67596ba4aa4995ca58704b9f8111a3015178b25
Angel Randall <drivenbysuccess21@gmail.com>
2020-04-23 21:38:45 -0500

    added functionality , routes, post and get , put , apis,

---
+++ routes/api.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+router.post("/api/workouts", (req, res) => {
+  Workout.create()
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+router.put("/api/workouts/:id", ({body,params}, res) => {
+  Workout.findByIdAndUpdate(
+    params.id, 
+    {$push: {exercises:body}},
+      {new: true}
+    )
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+router.post("/api/workouts", (req, res) => {
+  Workout.create()
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
